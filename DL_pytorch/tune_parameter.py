a76209d533a83b2536cbce97f33b9f2598344867
rock-paper-cissors <qiang_yuting@163.com>
2020-11-12 06:24:51 +0800

    deal exception

--- DL_pytorch/tune_parameter.py
+++ DL_pytorch/tune_parameter.py
@@ -16,18 +16,20 @@ def manual_tune(args, res_keys, npdtypes):
         values.append(v)
     keys += res_keys
     record_list = []
-    for c in itertools.product(*values):
-        print('Try this setting : ', dict(zip(keys, c)))
-        settings.fix_settings(dict(zip(keys, c)))
-        train.main(settings.params)
-        test.main(settings.params)
-        res = quantitive_evaluate.main(settings.params, 'relpred')
-        record_list.append(tuple([*c, *[res[k] for k in res_keys]]))
-    results = pd.DataFrame(np.array(
-        record_list,
-        dtype=[(keys[i], npdtypes[i]) for i in range(len(keys))]
-        ))
-    results.to_csv(os.path.join('expr_log.csv'))
+    try:
+        for c in itertools.product(*values):
+            print('Try this setting : ', dict(zip(keys, c)))
+            settings.fix_settings(dict(zip(keys, c)))
+            train.main(settings.params)
+            test.main(settings.params)
+            res = quantitive_evaluate.main(settings.params, 'relpred')
+            record_list.append(tuple([*c, *[res[k] for k in res_keys]]))
+        results = pd.DataFrame(np.array(
+            record_list,
+            dtype=[(keys[i], npdtypes[i]) for i in range(len(keys))]
+            ))
+    finally:
+        results.to_csv(os.path.join('expr_log.csv'))
 
 if __name__ == '__main__':
     
